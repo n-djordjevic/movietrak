c3c53be6d2f1af1bd387c90f6dbcf4ecda658edb
Nype <nikola.n.djordjevic98@gmail.com>
2021-01-27 00:30:24 +0100

    Adding bootstrap to landing page

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>MovieTRAK</title>
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <?php
 
@@ -17,7 +18,7 @@ include "database.php";
 <body>
     <h1>Dobrodosli na MovieTRAK</h1>
 
-    <a href="view.php">PRITISNI</a>
+    <a class="btn btn-primary" href="view.php" role="button">See your track record</a>
 
   
 </body>
